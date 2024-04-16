3f220fdc964e9930752f1a7f6ab2d25a95eafc51
ralph yozzo <ralph@brooklynmarathon.com>
2024-04-15 20:38:29 -0400

    ralph-desktop

--- public/javascript/apptest.js
+++ public/javascript/apptest.js
@@ -68,6 +68,7 @@ firebase.auth().onAuthStateChanged((firebaseUser) => {
   if (firebaseUser) {
     document.querySelector('#loader').style.display = 'none';
     document.querySelector('main').style.display = 'block';
+    document.querySelector('#useremail').innerHTML = `[${firebaseUser.email}]`;
     currentUser = firebaseUser.uid;
     startDataListeners();
   } else {
